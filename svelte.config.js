0a1e4776fae87a5356cb8328e4ac775a68b8dd7f
Jamie Shepherd <itsjamieshepherd@gmail.com>
2023-03-25 17:25:14 -0700

    Use threlte 6

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,12 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-import { preprocessThrelte } from '@threlte/preprocess';
-import seqPreprocessor from 'svelte-sequential-preprocessor';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     kit: {
         adapter: adapter(),
     },
-    preprocess: seqPreprocessor([vitePreprocess(), preprocessThrelte()]),
+    preprocess: [vitePreprocess()],
 };
 
 export default config;
