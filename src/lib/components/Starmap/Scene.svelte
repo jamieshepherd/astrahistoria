39a267b4a4547e99c26f232800c2616390101818
Jamie Shepherd <itsjamieshepherd@gmail.com>
2023-03-27 14:58:55 -0700

    Use a cute texture

--- src/lib/components/Starmap/Scene.svelte
+++ src/lib/components/Starmap/Scene.svelte
@@ -1,14 +1,32 @@
 <script>
+    import starImage from '$lib/assets/textures/star.png';
     import { T, useFrame } from '@threlte/core';
-    import { interactivity } from '@threlte/extras';
+    import { interactivity, useTexture } from '@threlte/extras';
     import { spring } from 'svelte/motion';
+    import * as THREE from 'three';
+
+    const galaxySeed = 1337;
+    const scale = spring(1);
 
     interactivity();
-    const scale = spring(1);
     let rotation = 0;
     useFrame((state, delta) => {
-        rotation += delta / 8;
+        rotation += delta / 128;
     });
+
+    let bufferGeometry;
+    const starCount = 5000;
+    const positions = new Float32Array(starCount * 3);
+
+    for (let i = 0; i < starCount * 3; i++) {
+        positions[i] =
+            (THREE.MathUtils.seededRandom(i * galaxySeed) - 0.5) * 10;
+    }
+
+    $: bufferGeometry?.setAttribute(
+        'position',
+        new THREE.BufferAttribute(positions, 3)
+    );
 </script>
 
 <T.PerspectiveCamera
@@ -20,8 +38,16 @@
 />
 
 <T.Points rotation.y={rotation} position.y={1} scale={$scale}>
-    <T.SphereBufferGeometry args={[1, 32, 32]} />
-    <T.PointsMaterial size={0.02} sizeAttenuation={true} />
+    <T.BufferGeometry bind:ref={bufferGeometry} />
+
+    {#await useTexture(starImage) then texture}
+        <T.PointsMaterial
+            size={0.02}
+            sizeAttenuation={true}
+            transparent={true}
+            alphaMap={texture}
+        />
+    {/await}
 </T.Points>
 
 <T.DirectionalLight position={[3, 10, 7]} />
